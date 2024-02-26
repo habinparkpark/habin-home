a3de46c0767b9df0e0710baf92992e7abb27acfb
habinparkpark <159488836+habinparkpark@users.noreply.github.com>
2024-02-25 23:55:21 -0500

    commit

--- global.js
+++ global.js
@@ -3,3 +3,19 @@ console.log("IT’S ALIVE!");
 function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+	{url:"", title: "Home"},
+	{url:"", title: "Home"},
+	{url:"", title: "Home"},
+	{url:"", title: "Home"},
+]
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+	let url = p.url;
+	let title = p.title;
+}
+nav.insertAdjacentHTML("beforeend", '<a ref="${ url }">${title}</a>');
