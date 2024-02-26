338a77c448072d629842985b64e19c7c2b5aa837
habinparkpark <159488836+habinparkpark@users.noreply.github.com>
2024-02-26 00:18:07 -0500

    commit

--- global.js
+++ global.js
@@ -4,12 +4,13 @@ function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
 
+
 let pages = [
 	{url:"", title: "Home"},
 	{url:"projects/", title: "Projects"},
 	{url:"contact/", title: "Contact Habin"},
 	{url:"about/", title: "About Habin"},
-]
+];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
